d4f10e0c0c2503e16d8e8a308fc1f994de77357f
ShizLo <pavelshizo@mail.ru>
2023-01-21 18:06:55 +0300

    gulp

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,17 @@
-import * as flsFunctions from "./modules/functions.js";
+import * as flsFunctions from "./modules/functions.js"
 
-flsFunctions.isWebp();
+flsFunctions.isWebp()
 
-import Swiper, { Navigation, Pagination } from 'swiper'
+import Swiper, { Navigation, Pagination } from "swiper"
 
-const swiper = new Swiper();
+const swiper = new Swiper()
 
+//Бургер
+const iconMenu = document.querySelector(".menu__icon")
+if (iconMenu) {
+  const menuBody = document.querySelector(".menu__body")
+  iconMenu.addEventListener("click", function (e) {
+    iconMenu.classList.toggle("_active")
+    menuBody.classList.toggle("_active")
+  })
+}
